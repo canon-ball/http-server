f2fe43a702b19c1f692ba723e0cac98570866741
canon-ball <canon_ball@mail.ru>
2019-10-31 10:46:08 +0300

    Upgrade shell scripts

--- avito.py
+++ avito.py
@@ -4,15 +4,19 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/api/v1/get_rabbit')
-def get_rabbit():
-    return render_template('get_rabbit.html')
-
-
 @app.route('/api/v1/get_mongo')
 def get_mongo():
-    return render_template('get_mongo.html')
+    with open('get_mongo.sh', 'r') as f:
+        content = f.read()
+    return render_template('content.html', text=content)
+
+
+@app.route('/api/v1/get_rabbit')
+def get_rabbit():
+    with open('get_rabbit.sh', 'r') as f:
+        content = f.read()
+    return render_template('content.html', text=content)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
